be2131b591939e72fe634cf950dab350be748cbe
Ristea Stefan <stefanristea1@yahoo.com>
2020-03-11 19:40:13 +0200

    Initial visual studio project

---
+++ IRProject/IRProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IRProject.Startup))]
+namespace IRProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
